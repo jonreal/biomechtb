0c40e11674719041c0bf20c46ce6ee9e18db8bdf
JR:alphaPlus <jonreal731@gmail.com>
2016-06-27 16:41:14 -0700

    Updated ILC for prosthetic experiment, -- adaptive learning

---
+++ ilc/adaptiveILC.m
@@ -0,0 +1,139 @@
+function S_k = adaptiveILC(yd_k, y_k, G_hat_inv, S_km1, varargin)
+% adaptiveILC Iterative learning control with adaptive learning gains.
+%             Currently works only in 1-D.
+%
+%   First iteration:
+%   S_0 = adaptiveILC(yd_k, y_k, G_hat_inv, 0, 'init', [gain,maxharmonic])
+%
+%
+%   All others:
+%   S_k = adaptiveILC(yd_k,y_k,G_hat_inv,S_km1)
+%
+%                         yd_k  -  k_th desired output
+%                         y_k   -  k_th output
+%                         G_hat_inv - inverse model (constant only!)
+%                         S_km1   -  {k-1}_th stucture
+%
+%                         S_k : Yd_k     - k_th desired output
+%                               yd_k     - k_th desired output (time domain)
+%                               Y_k      - k_th ouput
+%                               y_k      - k_th ouput (time domain)
+%                               rho_k    - k_th learning gains
+%                               E_bar_k  - k_th update error
+%                               U_bar_k  - k_th update input
+%                               E_k      - k_th error
+%                               U_kp1    - (k+1)_th input
+%                               u_kp1    - (k+1)_th input (time domain)
+%                               e_k_1    - k_th norm(yd_k - y_k,1)
+%                               e_k_2    - k_th norm(yd_k - y_k,2)
+%
+% Example:
+%   S_0 = adaptiveILC(yd_0, y_0, G_hat_inv, 0, 'init', [gain, maxharmonic]);
+%   S_1 = adaptiveILC(yd_1, y_1, G_hat_inv, S_0);
+%   S_2 = adaptiveILC(yd_2, y_2, G_hat_inv, S_1);
+%   .
+%   .
+%   .
+%   S_k = adaptiveILC(yd_k, y_k, G_hat_inv, S_km1);
+
+  init = 0;
+  nVarArgs = length(varargin);
+  for i=1:2:nVarArgs
+    switch varargin{i}
+      case 'init'
+        init = 1;
+        gain = varargin{i+1}(1);
+        maxharmonic = varargin{i+1}(2);
+      otherwise
+        fprintf('\n%s option not found!\n',varargin{i});
+        return
+    end
+  end
+
+  % Defaults
+  alpha = 2;
+
+  % Exclude last point (ie 100% sample == 0% sample)
+  yd_k = yd_k(1:end-1);
+  y_k = y_k(1:end-1);
+  L = numel(yd_k);
+  f = (0:L/2);
+  f = f(:);
+
+  % FFTs
+  Yd_k = fft(yd_k);
+  Yd_k = Yd_k(1:L/2+1);
+
+  Y_k = fft(y_k);
+  Y_k = Y_k(1:L/2+1);
+
+  % Error
+  E_k = Yd_k - Y_k;
+  e_k_1 = norm(yd_k - y_k,1);
+  e_k_2 = norm(yd_k - y_k,2);
+
+  % Initialize S_0 if flag is set
+  if (init)
+
+    % Init rho
+    rho_k = gain .* ones(numel(f),1);
+    rho_k(maxharmonic+2:end) = 0;
+
+    % First learning iteration
+    U_kp1 = rho_k .* G_hat_inv .* E_k;
+    u_kp1 = ifft([U_kp1; conj(flipud(U_kp1(2:end-1)))]);
+
+    % S_0 struct.
+    S_0.f = f;
+    S_0.Yd_k = Yd_k;
+    S_0.yd_k = yd_k;
+    S_0.Y_k = Y_k;
+    S_0.y_k = y_k;
+    S_0.rho_k = rho_k;
+    S_0.E_bar_k = zeros(numel(f),1);
+    S_0.U_bar_k = zeros(numel(f),1);
+    S_0.E_k = E_k;
+    S_0.U_kp1 = U_kp1;
+    S_0.u_kp1 = u_kp1;
+    S_0.e_k_1 = e_k_1;
+    S_0.e_k_2 = e_k_2;
+
+    S_k = S_0;
+    return
+  end
+
+  % Unwrap input struct.
+  rho_km1 = S_km1.rho_k;
+  E_bar_km1 = S_km1.E_bar_k;
+  U_bar_km1 = S_km1.U_bar_k;
+  U_k = S_km1.U_kp1;
+  E_km1 = S_km1.E_k;
+
+  % Find at what frequencies the magnitude of error has increased/decreased
+  E_incr = abs(E_k) > abs(E_km1);
+  E_decr = ~(E_incr);
+
+  % Updates
+  rho_k = (E_incr .* rho_km1) / alpha + (E_decr .* rho_km1);
+  E_bar_k = (E_decr .* E_bar_km1) + (E_decr .* E_k);
+  U_bar_k = (E_decr .* U_bar_km1) + (E_decr .* U_k);
+
+  U_kp1 = U_bar_k + rho_k .* G_hat_inv .* E_bar_k;
+  u_kp1 = ifft([U_kp1; conj(flipud(U_kp1(2:end-1)))]);
+
+  % Pack S_kp1 struct.
+  S_k.f = f;
+  S_k.Yd_k = Yd_k;
+  S_k.yd_k = yd_k;
+  S_k.Y_k = Y_k;
+  S_k.y_k = y_k;
+  S_k.rho_k = rho_k;
+  S_k.E_bar_k = E_bar_k;
+  S_k.U_bar_k = U_bar_k;
+  S_k.E_k = E_k;
+  S_k.U_kp1 = U_kp1;
+  S_k.u_kp1 = u_kp1;
+  S_k.e_k_1 = e_k_1;
+  S_k.e_k_2 = e_k_2;
+
+end
---
+++ ilc/ilc_poweredAnkle_tui.m
@@ -0,0 +1,315 @@
+function rtn = ilc_poweredAnkle_tui(varargin)
+% ilc_poweredAnkle_tui  Text based user interface for iterative learning
+%                       control.
+%
+%   ilc_poweredAnkle_tui() default configuration.
+%
+%   ilc_poweredAnkle_tui(Name,Value, ...) Name, Value pairs for adjustable
+%   settings.
+%
+%   Parameters: (defaults)
+%
+%   gain              -  (1), learning gain, value is positive numerical value
+%   stackmethod       -  (vicon), averaging source, value can be [vicon | embedded]
+%   maxharmonic       -  (10), max harmonic to learn, positive interger
+%   torque2current    -  (0.4), torque to current confersion factor, numerical value
+%                         this value is the inverse plant model
+%   smoothing         -  ([95,5])traj. smoothing, value is 2-element vector [b,a]
+%                         b - % gait to start smoothing
+%                         a - % gait to stop smoothing
+%                         b > a, (ie, b=90%, a=10%, smooth from 90% - 10% gait)
+%   mass              -  (85) subject mass
+%   maxcurrent        -  (20) saturation current, numerical value
+%   prostheticSide    -  (left) side with prosthetic
+%
+  nVarArgs = length(varargin);
+
+  % Defauts Settings
+  gain = 1;
+  stackmethod = 'vicon';
+  maxharmonic = 10;
+  torque2current = 0.4;
+  smooth_vector = [95,5];
+  mass = 85;
+  maxcurrent = 20;
+  prostheticSide = 'left';
+  gaitCycle = 0:0.1:(100 - 0.1);
+
+  for i=1:2:nVarArgs
+    switch varargin{i}
+      case 'prostheticSide'
+        prostheticSide = varargin{i+1};
+      case 'maxcurrent'
+        maxcurrent = varargin{i+1}
+      case 'mass'
+        mass = varargin{i+1};
+      case 'stackmethod'
+        stackmethod = varargin{i+1};
+      case 'maxharmonic'
+        maxharmonic = varargin{i+1};
+      case 'torque2current'
+        torque2current = varargin{i+1};
+      case 'gain'
+        gain = varargin{i+1};
+      case 'smoothing'
+        smooth_vector = varargin{i+1};
+      otherwise
+        fprintf('\n%s option not found!\n',varargin{i})
+        rtn = [];
+        return
+    end
+  end
+
+  % Filter for uff smoothing
+  [b,a] = butter(1,(2*10)/1000);
+
+  % Figures
+  h_torqe = figure;
+  h_error_frq = figure;
+  h_rho = figure;
+  h_error_trial = figure;
+  h_torque_only = figure;
+
+  fprintf('\n----------------------\n')
+  fprintf('Iterative Learning UI\n')
+  fprintf('-----------------------\n')
+  fprintf('\nSettings:\n')
+  fprintf('\tProsthetic Side = %s\n',prostheticSide);
+  fprintf('\tStack Method = %s\n',stackmethod)
+  fprintf('\tMaximum harmonic = %i\n',maxharmonic)
+  fprintf('\tAnkle torque to motor current = %.3f\n',torque2current)
+  fprintf('\tLearning Gain = %.3f\n',gain)
+  fprintf('\tSmoothing = %i%% - %i%%\n', smooth_vector)
+
+  k=1;
+  while(1)
+    fprintf('\n................\n')
+    fprintf('Iteration %i\n',k - 1);
+    fprintf('................\n')
+
+    % Get k trial
+    while(1)
+      fprintf('\n\t');
+      trial = input('Enter trial name: ','s');
+      fprintf(['\n\t',trial]);
+      usr_input = input(' - Is this correct? [y/n] ','s');
+      if strcmp('y',usr_input)
+        break;
+      end
+    end
+
+    % Data Analysis
+    fprintf('\n\tParsing data...\n');
+    rtn.T{k} = vicon_process_trial(trial,'stackmethod',stackmethod);
+
+    % Desired output and output
+    if strcmp(prostheticSide,'left')
+      yd_k = rtn.T{k}.stats.r.RAnkleMoment.X(:,2);
+      y_k = rtn.T{k}.stats.l.LAnkleMoment.X(:,2);
+    else
+      yd_k = rtn.T{k}.stats.l.LAnkleMoment.X(:,2);
+      y_k = rtn.T{k}.stats.r.RAnkleMoment.X(:,2);
+    end
+
+    while(1)
+
+      % If first iteration, initialize learning
+      if (k==1)
+        fprintf('\n\tInitializing learning structures...\n');
+        rtn.S{k} = adaptiveILC(yd_k,y_k,1,0, ...
+                               'init',[gain,maxharmonic]);
+      else
+        fprintf('\n\tLearning...\n');
+        rtn.S{k} = adaptiveILC(yd_k,y_k,1,rtn.S{k-1});
+      end
+
+      % Filter uff
+      u_k_filt = rtn.S{k}.u_kp1;
+      u_k_filt(1:(round(smooth_vector(2)*10))) = 0;
+      u_k_filt(round(smooth_vector(1)*10):end) = 0;
+      u_k_filt = filtfilt(b,a,u_k_filt);
+
+      u1 = mass*torque2current*rtn.S{k}.u_kp1;
+      u2 = mass*torque2current*u_k_filt;
+
+      if sum(abs(u2) > maxcurrent) > 0
+        fprintf('\n---Motor will saturate!---\n');
+        u2(u2 > maxcurrent) = maxcurrent;
+        u2(u2 < -maxcurrent) = -maxcurrent;
+      end
+
+      figure(h_torque_only); clf; hold all;
+        plot(gaitCycle,u1,'k');
+        plot(gaitCycle, u2,'r');
+        title('Learned Torque Signal','fontsize',20);
+        xlabel('% gait','fontsize',20);
+        ylabel('Current (A)','fontsize',20);
+
+      fprintf('\n\tSignal learned!\n');
+      fprintf('\t\tIs this signal sufficient?')
+      usr_input = input('[y|n] ', 's');
+      if strcmp(usr_input,'n')
+        while(1)
+          fprintf('\t\tDo you want to change a parameter?\n')
+          fprintf('\t\t 1 - gain\n')
+          fprintf('\t\t 2 - earlysmooth\n')
+          fprintf('\t\t 3 - latesmooth\n');
+          fprintf('\t\t 4 - done\n')
+          while(1)
+            fprintf('\t\t\t');
+            usr_input = input('Enter response: ');
+            if isscalar(usr_input)
+              break;
+            end
+          end
+
+          switch usr_input
+            case 1
+              fprintf('\t\t\t');
+              usr_input = input('Enter new gain value: ');
+              gain = usr_input;
+            case 2
+              fprintf('\t\t\t');
+              usr_input = input('Enter new earlysmooth: ');
+              smooth_vector(2) = usr_input;
+            case 3
+              fprintf('\t\t\t');
+              usr_input = input('Enter new latesmooth: ');
+              smooth_vector(1) = usr_input;
+            case 4
+              fprintf('\tLearning Gain = %.3f\n',gain)
+              fprintf('\tSmoothing = %i%% - %i%%\n', smooth_vector)
+              break;
+            otherwise
+              warning('Not an option');
+          end
+        end
+      else
+        break;
+      end
+    end
+    rtn.S{k}.u_kp1_filt = u_k_filt;
+
+    % Torque
+    figure(h_torqe); clf; hold all;
+      for i=1:k;
+        plot3(gaitCycle.*0 + (i-1), ...
+              gaitCycle, ...
+              rtn.S{i}.yd_k,'k')
+        plot3(gaitCycle.*0 + (i-1), ...
+              gaitCycle, ...
+              rtn.S{i}.y_k,'r')
+        plot3(gaitCycle.*0 + (i-1), ...
+              gaitCycle, ...
+              rtn.S{i}.u_kp1_filt,'g')
+      end
+      grid on
+      if(k>1)
+        set(gca,'Xtick',[1:k])
+        xlim([0,k-1])
+      end
+      xlabel('Iteration','fontsize',20);
+      ylabel('% gait','fontsize',20);
+      zlabel('Nm/kg','fontsize',20);
+      title('Torque','fontsize',20);
+      view(40,30);
+
+    % Yd and Y plots
+    figure(h_error_frq); clf; hold all;
+      for i=1:k;
+        stem3(rtn.S{i}.f(1:maxharmonic+1).*0 + (i-1), ...
+              rtn.S{i}.f(1:maxharmonic+1), ...
+              abs(rtn.S{i}.Yd_k(1:maxharmonic+1)),'k')
+        stem3(rtn.S{i}.f(1:maxharmonic+1).*0 + (i-1), ...
+              rtn.S{i}.f(1:maxharmonic+1), ...
+              abs(rtn.S{i}.Y_k(1:maxharmonic+1)),'r')
+        stem3(rtn.S{i}.f(1:maxharmonic+1).*0 + (i-1), ...
+              rtn.S{i}.f(1:maxharmonic+1), ...
+              abs(rtn.S{i}.E_k(1:maxharmonic+1)),'g')
+      end
+      grid on
+      ylim([0,10])
+      if(k>1)
+        set(gca,'Xtick',[1:k])
+        xlim([0,k-1])
+      end
+      xlabel('Iteration','fontsize',20);
+      ylabel('f/f_0','fontsize',20);
+      zlabel('Mag','fontsize',20);
+      title('Error','fontsize',20);
+      view(40,30);
+
+    % Rho
+    figure(h_rho); clf; hold all;
+      for i=1:k;
+        stem3(rtn.S{i}.f(1:maxharmonic+1).*0 + (i-1), ...
+              rtn.S{i}.f(1:maxharmonic+1), ...
+              abs(rtn.S{i}.rho_k(1:maxharmonic+1)),'k')
+      end
+      grid on
+      ylim([0,10])
+      if(k>1)
+        set(gca,'Xtick',[1:k])
+        xlim([0,k-1])
+      end
+      xlabel('Iteration','fontsize',20);
+      ylabel('f/f_0','fontsize',20);
+      zlabel('Mag','fontsize',20);
+      title('Learning Weights','fontsize',20);
+      view(40,30);
+
+    % Error
+    figure(h_error_trial); clf; hold all;
+      for i=1:k;
+        stem((i-1),rtn.S{i}.e_k_1,'k')
+        stem((i-1),rtn.S{i}.e_k_2,'r')
+      end
+      grid on
+      if(k>1)
+        set(gca,'Xtick',[1:k])
+        xlim([0,k-1])
+      end
+      xlabel('Iteration','fontsize',20);
+      ylabel('Error','fontsize',20);
+      title('Error','fontsize',20);
+
+    fprintf('\n\t');
+    fprintf('Write feedforward signal to file?');
+    usr_input = input('[y|n]','s');
+    fprintf('\n');
+
+    if strcmp(usr_input,'y')
+      writeFlag = 1;
+      file = ['./','uff_',num2str(k)];
+      if exist(file,'file') == 2
+        fprintf('\n\t\tFile already exist');
+        usr_input = input(' overwrite? [y/n]','s');
+        if strcmp('y',usr_input)
+          writeFlag = 1;
+        else
+          writeFlag = 0;
+          fprintf('\n\tSignal not written to file.\n');
+        end
+      end
+      if writeFlag
+        fid = fopen(['./','uff_',num2str(k)],'w');
+        fprintf(fid,'%f\n',rtn.S{k+1}.u_kp1_filt);
+        fclose(fid);
+      end
+    end
+
+    fprintf('\n\t');
+    usr_input = input('Continue with next k? [y/n]','s');
+    if strcmp('n',usr_input)
+      fprintf('\t\t')
+      usr_input = input('Redo k? [y/n] (n will exit ui)','s');
+      if strcmp('y',usr_input)
+        continue;
+      end
+      break;
+    end
+
+    % Increment
+    k = k + 1;
+  end
+end
